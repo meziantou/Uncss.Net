3a31df4343e9f29a245f0210f962bde63169d561
Gérald Barré <meziantou@live.fr>
2018-07-21 11:20:46 -0400

    Add project files.

---
+++ Uncss.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uncss.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

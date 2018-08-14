3f7064e150ff105e59705f95849e625f8e035fa2
meziantou <meziantou@live.fr>
2018-08-13 21:52:18 -0400

    Handle pseudo-selector

--- Uncss.Net/Program.cs
+++ Uncss.Net/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using AngleSharp;
 using AngleSharp.Dom.Css;
 using AngleSharp.Dom.Events;
+using AngleSharp.Extensions;
+using AngleSharp.Parser.Css;
 using Newtonsoft.Json;
 
 namespace Uncss.Net
@@ -31,6 +33,11 @@ namespace Uncss.Net
                             return true;
                         }
 
+                        if (path.EndsWith("prism.min.css", StringComparison.OrdinalIgnoreCase))
+                        {
+                            return false;
+                        }
+
                         if (path.EndsWith(".css", StringComparison.OrdinalIgnoreCase))
                         {
                             return true;
@@ -43,56 +50,64 @@ namespace Uncss.Net
                 .WithLocaleBasedEncoding();
 
             // Process the urls set in the command line
-            Parallel.ForEach(args.Where(_ => !string.IsNullOrEmpty(_)), url =>
+            Parallel.ForEach(args.Where(_ => !string.IsNullOrEmpty(_)), new ParallelOptions { MaxDegreeOfParallelism = -1 }, url =>
             {
-                // Create a new BrowsingContext to download and parse the page
-                var browsingContext = BrowsingContext.New(config);
-                browsingContext.Requesting += (sender, ev) => Console.WriteLine("Requesting: " + ((RequestEvent)ev).Request.Address);
-                var u = Url.Create(url);
-                if (u == null)
+                try
                 {
-                    return;
-                }
-
-                // Open the page
-                var openTask = browsingContext.OpenAsync(u);
-                if (openTask == null)
-                {
-                    return;
-                }
-
-                var document = openTask.Result;
-
-                // Get all stylesheets of the document
-                foreach (var stylesheet in document.StyleSheets.OfType<ICssStyleSheet>())
-                {
-                    foreach (var cssRule in stylesheet.Rules.OfType<ICssStyleRule>()) // TODO handle @ rule and remove ::before, ::after, :hover, ...
+                    // Create a new BrowsingContext to download and parse the page
+                    var browsingContext = BrowsingContext.New(config);
+                    browsingContext.Requesting += (sender, ev) => Console.WriteLine("Requesting: " + ((RequestEvent)ev).Request.Address);
+                    var u = Url.Create(url);
+                    if (u == null)
                     {
-                        foreach (var selector in GetAllSelectors(cssRule.Selector))
-                        {
-                            var r = new Rule(stylesheet.Href, selector.Text, cssRule.SelectorText);
-                            r = rules.GetOrAdd(r, r);
-                            if (r.Used)
-                            {
-                                continue;
-                            }
+                        return;
+                    }
 
-                            // Check if the rule match an element of the document
-                            var match = document.QuerySelector(r.Selector);
-                            if (match != null)
+                    // Open the page
+                    var openTask = browsingContext.OpenAsync(u);
+                    if (openTask == null)
+                    {
+                        return;
+                    }
+
+                    var document = openTask.Result;
+
+                    // Get all stylesheets of the document
+                    foreach (var stylesheet in document.StyleSheets.OfType<ICssStyleSheet>())
+                    {
+                        foreach (var cssRule in GetAllRules(stylesheet))
+                        {
+                            foreach (var selector in GetAllSelectors(cssRule.Selector))
                             {
-                                lock (r)
+                                var querySelector = GetSelector(selector);
+                                var r = new Rule(stylesheet.Href, selector.Text, querySelector.Text, cssRule.SelectorText);
+                                r = rules.GetOrAdd(r, r);
+                                if (r.Used)
                                 {
-                                    if (!r.Used)
-                                    {
-                                        r.UsageUrl = url;
-                                    }
+                                    continue;
                                 }
 
-                                break;
+                                // Check if the rule match an element of the document
+                                var match = document.QuerySelector(r.QuerySelector);
+                                if (match != null)
+                                {
+                                    lock (r)
+                                    {
+                                        if (!r.Used)
+                                        {
+                                            r.UsageUrl = url;
+                                        }
+                                    }
+
+                                    break;
+                                }
                             }
                         }
                     }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
                 }
             });
 
@@ -105,25 +120,53 @@ namespace Uncss.Net
             File.WriteAllText("output.json", JsonConvert.SerializeObject(rules.Keys, Formatting.Indented));
         }
 
+        private static IEnumerable<ICssStyleRule> GetAllRules(ICssNode node)
+        {
+            if (node is ICssStyleRule a)
+            {
+                yield return a;
+            }
+
+            foreach (var child in node.Children)
+            {
+                foreach (var item in GetAllRules(child))
+                {
+                    yield return item;
+                }
+            }
+        }
 
         // Get all selectors of a CSS rule
         // ".a, .b, .c" contains 3 selectors ".a", ".b" and ".c"
         private static IEnumerable<ISelector> GetAllSelectors(ISelector selector)
         {
-            if (selector is IEnumerable<ISelector> selectors)
+            if (selector.GetType().Name == "ListSelector")
             {
-                foreach (var innerSelector in selectors)
+                foreach (var s in ((IEnumerable<ISelector>)selector))
                 {
-                    foreach (var s in GetAllSelectors(innerSelector))
+                    foreach (var a in GetAllSelectors(s))
                     {
-                        yield return s;
+                        yield return a;
                     }
                 }
             }
-            else
-            {
-                yield return selector;
-            }
+
+            yield return selector;
+            yield break;
+        }
+
+        private static ISelector GetSelector(ISelector selector)
+        {
+            var text = selector.ToCss()
+                .Replace("::after", "")
+                .Replace(":after", "")
+                .Replace("::before", "")
+                .Replace(":before", "")
+                .Replace(":active", "")
+                .Replace(":focus", "")
+                .Replace(":hover", "");
+
+            return new CssParser().ParseSelector(text);
         }
     }
 
@@ -131,14 +174,16 @@ namespace Uncss.Net
     {
         public string StylesheetUrl { get; }
         public string Selector { get; }
+        public string QuerySelector { get; }
         public string RuleText { get; }
         public string UsageUrl { get; set; }
         public bool Used => UsageUrl != null;
 
-        public Rule(string stylesheetUrl, string selector, string ruleText)
+        public Rule(string stylesheetUrl, string selector, string querySelector, string ruleText)
         {
             StylesheetUrl = stylesheetUrl ?? throw new ArgumentNullException(nameof(stylesheetUrl));
             Selector = selector ?? throw new ArgumentNullException(nameof(selector));
+            QuerySelector = querySelector ?? throw new ArgumentNullException(nameof(querySelector));
             RuleText = ruleText ?? throw new ArgumentNullException(nameof(ruleText));
         }
 
